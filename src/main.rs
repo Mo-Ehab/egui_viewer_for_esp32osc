bdd778b28077b011ede23771621707e12cc4c207
Mo-ehab <mo7medehab.16@gmail.com>
2024-04-08 12:53:47 +0300

    Improved bounding and speed

--- src/main.rs
+++ src/main.rs
@@ -38,12 +38,7 @@ fn main() -> Result<(), eframe::Error> {
 
     // UART receive thread
     thread::spawn(move || {
-        loop{
-            uart_data.viewerdata = main_receive.try_iter().last().unwrap_or( uart_data.viewerdata);
-            uart_data.uart_thread();
-            uart_send.send(uart_data.graph.clone()).expect("Channel died")
-        }
-
+            uart_data.uart_thread(uart_send, main_receive);
     });
 
     // Main (viewing) thread
@@ -55,7 +50,7 @@ fn main() -> Result<(), eframe::Error> {
                 graph: vec![[0.0, 0.0]],
                 bounded: true,
                 frame_history : Default::default(),
-                viewerdata: Viewerdata { xscale: 10.0, yscale: 10.0, frozen:true },
+                viewerdata: Viewerdata { xscale: 5.0, yscale: 5.0, frozen:false },
                 uart_receiver: uart_receive,
                 main_send: main_send,
                 freq : Freq::Off
@@ -86,54 +81,59 @@ impl Default for UARTdata {
     fn default() -> Self {
         Self {
             graph: vec![[0.0, 0.0]],
-            viewerdata: Viewerdata { xscale: 10.0, yscale: 10.0, frozen:true },
+            viewerdata: Viewerdata { xscale: 5.0, yscale: 5.0, frozen:false },
         }
     }
 }
 
-pub trait UARTthread {
-    fn uart_thread(&mut self) ;
+trait UARTthread {
+    fn uart_thread(&mut self, uart_send: Sender<Vec<[f64; 2]>>, main_receive: Receiver<Viewerdata>) -> ! ;
 }
 
 impl UARTthread for UARTdata {
 
-    fn uart_thread(&mut self) {
+    fn uart_thread(&mut self, uart_send: Sender<Vec<[f64; 2]>>, main_receive: Receiver<Viewerdata> ) -> ! {
+        thread::sleep(Duration::from_millis(1000));
+        let mut port = serialport::new("COM10", 921600 )
+            .timeout(Duration::from_millis(10))
+            .open_native().expect("Failed to open port");
 
-        let mut port = serialport::new("COM12", 921600 )
-            .timeout(Duration::from_millis(100))
-            .open().expect("Failed to open port"); // Connect to UART port
+        loop{
+ 
+            self.viewerdata = main_receive.try_iter().last().unwrap_or( self.viewerdata);
+            let mut serial_buf:Vec<u8> = vec![0; 100];
+            port.read(serial_buf.as_mut_slice()).expect("Found no data!"); // Read from UART to buffer
 
-        let mut serial_buf   =[0; 512];
-        port.read_exact(&mut serial_buf).expect("Found no UART_data!"); // Read from UART to buffer
+            let data_string = String::from_utf8_lossy(&serial_buf) ; //convert buffer data to one long string
 
-        let data_string = String::from_utf8_lossy(&serial_buf) ; //convert buffer data to one long string
-        let data_vector : Vec<&str> = data_string.split(", ").collect(); // Convert string data to array of strings
-        let number_data : Vec<f64>  = data_vector.iter().map(|&f| f.parse::<f64>().unwrap_or(0.0)).collect(); // Convert data to vector of float values
+            let data_vector : Vec<&str> = data_string.split(", ").collect(); // Convert string data to array of strings
+            let number_data : Vec<f64>  = data_vector.iter().map(|&f| f.parse::<f64>().unwrap_or(0.0)).collect(); // Convert data to vector of float values
 
-        // Update graph if UI not frozen 
-        if !self.viewerdata.frozen{
+            // Update graph if UI not frozen 
+            if !self.viewerdata.frozen{
 
-            for (i, item) in number_data.iter().enumerate(){
-                if i ==0 || i ==1 || i > (number_data.len() - 2){   // truncate readings at start and end of port reading because they give wrong readings
-                }
-                else{
-                    if self.graph.last().unwrap()[0] + (100.0/1000000.0) > self.viewerdata.xscale{ // Wrap data back to the start of graph
-                        self.graph.push([self.graph.last().unwrap()[0] + (100.0/1000000.0) , -1.0]);
-                        self.graph.push([0.0 + (100.0/1000000.0) , -1.0]);
-                    } 
+                for (i, item) in number_data.iter().enumerate(){
+                    if i ==0 || i ==1 || i > (number_data.len() - 2){   // truncate readings at start and end of port reading because they give wrong readings
+                    }
                     else{
-                        self.graph.push([self.graph.last().unwrap()[0] + (100.0/1000000.0) , item/1241.1]); // Append readings to graph, currently one reading every 500 microsecond
+                        if self.graph.last().unwrap()[0] + (150.0/1000000.0) > self.viewerdata.xscale{ // Wrap data back to the start of graph
+                            self.graph.push([self.graph.last().unwrap()[0] + (150.0/1000000.0) , -1.0]);
+                            self.graph.push([0.0 + (150.0/1000000.0) , -1.0]);
+                        } 
+                        else{
+                            self.graph.push([self.graph.last().unwrap()[0] + (150.0/1000000.0) , item/1240.0]); // Append readings to graph, currently one reading every 150 microsecond
+                        }
+                    }
+
+                    if self.viewerdata.xscale * (1000000.0/150.0) < self.graph.len() as f64{ // Remove old readings to save memory and compute
+                        self.graph.drain(..( self.graph.len() as f64 - self.viewerdata.xscale * (1000000.0/150.0)  )  as usize );
                     }
                 }
 
-                if self.viewerdata.xscale * (1000000.0/100.0) < self.graph.len() as f64{ // Remove old readings to save memory and compute
-                    self.graph.drain(..( self.graph.len() as f64 - self.viewerdata.xscale * (1000000.0/100.0)  )  as usize );
-                }
             }
+            uart_send.send(self.graph.clone()).expect("Channel died")
 
         }
-
-
     }
 
 }
@@ -208,13 +208,10 @@ impl eframe::App for MyApp {
                 });
             });
 
-
-
-            let inner = my_plot.show(ui, |plot_ui| {
+            let inner = my_plot.allow_drag(!self.bounded).allow_scroll(!self.bounded).allow_zoom(!self.bounded).show(ui, |plot_ui| {
                 if self.bounded{
                     plot_ui.set_plot_bounds(PlotBounds::from_min_max([ 0.0 ,0.0], [ self.viewerdata.xscale ,self.viewerdata.yscale]))
-                }
-
+                };
                 if self.freq == Freq::On{ // Basic try for fft based spectrum analyzer
                     let mut real_planner = RealFftPlanner::<f64>::new();
                     let r2c = real_planner.plan_fft_forward(self.graph.len());
